ee51efacf98def0e0e3eafff0108312ba0d66d65
Jack <Jack@thelondons.com>
2025-09-04 15:32:05 -0500

    ASDF

---
+++ Schedule_Creator_V2/Models/BuildScheduleLine.cs
@@ -0,0 +1,4 @@
+﻿namespace Schedule_Creator_V2.Models
+{
+    public record BuildScheduleLine(string monShift, string tuesShift, string wedShift, string thursShift, string friShift, string satShift, string sunShift);
+}
