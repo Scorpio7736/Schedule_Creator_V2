cc1123cfa794c876fb4cd78135cc44353f1a1758
Jack <Jack@thelondons.com>
2025-09-30 00:29:57 -0500

    Add collection hours page and menu entry

---
+++ Schedule_Creator_V2/Add_Collection.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Schedule_Creator_V2
+{
+    /// <summary>
+    /// Interaction logic for Add_Collection.xaml
+    /// </summary>
+    public partial class Add_Collection : Page
+    {
+        public Add_Collection()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Add persistence logic for collection hours.
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DayOfWeekComboBox.SelectedIndex = -1;
+            OpeningTimePicker.Value = null;
+            ClosingTimePicker.Value = null;
+        }
+    }
+}
