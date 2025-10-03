767b937d2045d176a8808656e9d088c17aca924a
Jack <Jack@thelondons.com>
2025-10-02 22:19:04 -0500

    Fix time picker enabling without nonexistent ItemsSource

--- Schedule_Creator_V2/Add_Job_Settings.xaml.cs
+++ Schedule_Creator_V2/Add_Job_Settings.xaml.cs
@@ -1,6 +1,8 @@
-using Schedule_Creator_V2.Models;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using TimePickerControl = Xceed.Wpf.Toolkit.TimePicker;
 
 namespace Schedule_Creator_V2
 {
@@ -9,12 +11,17 @@ namespace Schedule_Creator_V2
     /// </summary>
     public partial class Add_Job_Settings : Page
     {
+        private readonly IReadOnlyList<DayTimePickerGroup> _dayTimePickerGroups;
 
         public Add_Job_Settings()
         {
             InitializeComponent();
+            _dayTimePickerGroups = CreateDayTimePickerGroups();
 
-
+            foreach (DayTimePickerGroup group in _dayTimePickerGroups)
+            {
+                group.CheckBox.Click += DayCheckBox_Click;
+            }
         }
 
 
@@ -28,6 +35,71 @@ namespace Schedule_Creator_V2
 
         }
 
+
+        private void DayCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not CheckBox checkBox)
+            {
+                return;
+            }
+
+            foreach (DayTimePickerGroup group in _dayTimePickerGroups)
+            {
+                if (!ReferenceEquals(group.CheckBox, checkBox))
+                {
+                    continue;
+                }
+
+                bool isChecked = checkBox.IsChecked == true;
+
+                UpdateTimePicker(group.OpeningPicker, isChecked, TimeSpan.FromHours(9));
+                UpdateTimePicker(group.ClosingPicker, isChecked, TimeSpan.FromHours(17));
+
+                break;
+            }
+        }
+
+        private void UpdateTimePicker(TimePickerControl timePicker, bool isEnabled, TimeSpan defaultTimeOfDay)
+        {
+            timePicker.IsEnabled = isEnabled;
+
+            if (isEnabled)
+            {
+                if (timePicker.Value == null)
+                {
+                    timePicker.Value = CreateDefaultTime(defaultTimeOfDay);
+                }
+            }
+            else
+            {
+                timePicker.Value = null;
+            }
+        }
+
+        private static DateTime CreateDefaultTime(TimeSpan timeOfDay)
+        {
+            DateTime today = DateTime.Today;
+            DateTime defaultTime = today.Date + timeOfDay;
+
+            return defaultTime;
+        }
+
+        private IReadOnlyList<DayTimePickerGroup> CreateDayTimePickerGroups()
+        {
+            return new List<DayTimePickerGroup>
+            {
+                new(EveryDayCheckBox, EveryDayOpeningTimePicker, EveryDayClosingTimePicker),
+                new(MondayCheckBox, MondayOpeningTimePicker, MondayClosingTimePicker),
+                new(TuesdayCheckBox, TuesdayOpeningTimePicker, TuesdayClosingTimePicker),
+                new(WednesdayCheckBox, WednesdayOpeningTimePicker, WednesdayClosingTimePicker),
+                new(ThursdayCheckBox, ThursdayOpeningTimePicker, ThursdayClosingTimePicker),
+                new(FridayCheckBox, FridayOpeningTimePicker, FridayClosingTimePicker),
+                new(SaturdayCheckBox, SaturdayOpeningTimePicker, SaturdayClosingTimePicker),
+                new(SundayCheckBox, SundayOpeningTimePicker, SundayClosingTimePicker)
+            };
+        }
+
+        private sealed record DayTimePickerGroup(CheckBox CheckBox, TimePickerControl OpeningPicker, TimePickerControl ClosingPicker);
 
     }
 }
