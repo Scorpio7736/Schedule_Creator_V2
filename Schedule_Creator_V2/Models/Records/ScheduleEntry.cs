22cb9fa422a4eb2fcce735f33cdb6d125ca09a76
Jack <Jack@thelondons.com>
2025-10-23 14:16:56 -0500

    Remove unused support code

--- Schedule_Creator_V2/Models/Records/ScheduleEntry.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Schedule_Creator_V2.Models
-{
-    public record ScheduleEntry(string fName, string lName, string availTimes, string position, string certRange);
-}
--- Schedule_Creator_V2/Services/Messages.cs
+++ Schedule_Creator_V2/Services/Messages.cs
@@ -1,14 +1,8 @@
-﻿using Schedule_Creator_V2.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Schedule_Creator_V2.Models;
 using System.Windows;
 
 namespace Schedule_Creator_V2.Services
 {
-    
     internal class Messages
     {
         /// <summary>
@@ -19,18 +13,14 @@ namespace Schedule_Creator_V2.Services
         {
             MessageBox.Show(msg.message, msg.caption);
         }
+
         /// <summary>
-        /// Used to inform the user on an error. This includes a code for refrence and an error message that is specific to the messagebox.
+        /// Used to inform the user on an error. This includes a code for reference and an error message that is specific to the message box.
         /// </summary>
         /// <param name="error"></param>
         public static void Display(Error error)
         {
             MessageBox.Show($"Error {error.code} - {error.message}", "Error!");
-        }
-
-        public static void Display(Warning warning)
-        {
-            MessageBox.Show($"WARNING: {warning.msg}", "WARNING");
         }
     }
 }
@@ -38,12 +28,11 @@ namespace Schedule_Creator_V2.Services
 /* Error List:
  * 1000 - Null value error.
  * 1002 - missing functionality.
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
